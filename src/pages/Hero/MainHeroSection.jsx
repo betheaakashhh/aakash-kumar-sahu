a987869c9cfccec82b77b8bd289e860a8e4fee7b
Aakash sahu <sahu.aakash1008@gmail.com>
2025-12-28 05:25:32 +0530

    increased the floaitng particle2

--- src/pages/Hero/MainHeroSection.jsx
+++ src/pages/Hero/MainHeroSection.jsx
@@ -74,6 +74,25 @@ const MainHeroSection = () => {
             }}></div>
           ))}
         </div>
+        <div className="floating-particles">
+          {[...Array(120)].map((_, i) => (
+            <div key={i} className="particle" style={{
+              left: `${Math.random() * 1000}%`,
+              animationDelay: `${Math.random() * 1}s`,
+              animationDuration: `${15 + Math.random() * 10}s`
+            }}></div>
+          ))}
+        </div>
+        <div className="floating-particles">
+          {[...Array(800)].map((_, i) => (
+            <div key={i} className="particle" style={{
+              left: `${Math.random() * 100}%`,
+              right: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 1}s`,
+              animationDuration: `${15 + Math.random() * 10}s`
+            }}></div>
+          ))}
+        </div>
       </div>
 
       <div className="hero-container">
